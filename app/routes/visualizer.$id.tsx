a2194a845bd73be2fd4777470208ba2e3f2e2169
Joshkovu <joashkuteesa223@gmail.com>
2026-04-17 10:36:58 +0300

    Implement ai 2D images to 3D images

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,19 +1,119 @@
 
-import { useLocation } from "react-router"
-
+import { useLocation, useNavigate } from "react-router"
+import { useEffect, useRef, useState } from "react";
+import { generate3DView } from "../../lib/ai.action";
+import { Box,X,Download, Share2, RefreshCcw } from "lucide-react";
+import Button from "../../components/Button";
 const visualizerId = () => {
+  const navigate = useNavigate();
   const location = useLocation()
-  const {initialImage,name}= location.state || {};
+  const {initialImage,initialRender,name}= location.state || {};
+  const hasInitialGenerated = useRef(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [currentImage, setCurrentImage] = useState(initialRender ||null);
+  const handleBack = ()=> navigate("/");
+  const runGeneration = async () => {
+    if(!initialImage) return ;
+    try{
+      setIsProcessing(true);
+      const result = await generate3DView({sourceImage: initialImage});
+      if(result.renderedImage){
+        setCurrentImage(result.renderedImage);
+      }
+    }
+    catch(e){
+      console.error("Error during generation:", e);
+    }finally{
+      setIsProcessing(false);
+    }
+  }
+  useEffect(()=>{
+    if(!initialImage || hasInitialGenerated.current) return;
+    if(initialRender) {
+      setCurrentImage(initialRender);
+      hasInitialGenerated.current = true;
+      return;
+    }
+    hasInitialGenerated.current = true;
+    runGeneration();
+  }, [initialImage]);
+
+
   return (
-    <section>
-      <h1>{name || "Untitled Project"}</h1>
+    
+      
       <div className="visualizer">
-        {initialImage && (<div className="image-container">
-          <h2>Source Image</h2>
-          <img src={initialImage} alt="source" />
-        </div> ) }
+        <nav className="topbar">
+       <div className="brand">
+         <Box  className="logo"/>
+            <span className="name">
+                Roomify
+            </span>
+            <Button variant="ghost" size="sm" onClick={handleBack} className="exit">
+              <X className="icon"/>
+              Exit editor
+              </Button>
+            
+       </div>
+       </nav>
+       <section className="content">
+        <div className="panel">
+  <div className="panel-header">
+          <div className="panel-meta">
+            <p>Project</p>
+            <h2>{"Untitled Project"}</h2>
+            <p className="note">Created by You</p>
+          </div>
+          <div className="panel-actions">
+            <Button
+            size="sm"
+            onClick={()=>{}}
+            className="export"
+            disabled={!currentImage || isProcessing}
+            >
+              <Download className="w-4 h-4 mr-2"/>
+              Export
+
+            </Button>
+             <Button
+            size="sm"
+            onClick={()=>{}}
+            className="share"
+          
+            >
+              <Share2 className="w-4 h-4 mr-2"/>
+              Share
+
+            </Button>
+
+          </div>
+        </div>
+
+        <div className={`render-area ${isProcessing ? "is-processing" : ""}`}>
+          {currentImage ? (
+            <img src={currentImage} alt="Rendered ai image" className="render-img"/>
+          ) : (
+           <div className="render-placeholder">
+            {initialImage && (
+              <img src={initialImage} alt="Initial image" className="render-fallback" />
+            ) }
+           </div>
+          )}
+  {isProcessing && (
+    <div className="render-overlay">
+      <div className="rendering-card">
+        <RefreshCcw className="spinner"/>
+        <span className="title">Rendering ...</span>
+        <span className="subtitle">Generating your 3D visualization</span>
       </div>
-    </section>
+    </div>
+  )}
+        </div>
+        </div>
+        
+       </section>
+      </div>
+   
   )
 }
 
